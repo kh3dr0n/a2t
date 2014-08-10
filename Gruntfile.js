16cfd4d0bb9a28cded6a088220c5e11fb1e296e8
Marwen Kheder <marwen.kheder@gmail.com>
2014-08-10 16:07:48 +0100

    v3

--- Gruntfile.js
+++ Gruntfile.js
@@ -408,7 +408,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('build', [
     'clean:dist',
-    'wiredep',
+    // 'wiredep',
     'useminPrepare',
     'concurrent:dist',
     'autoprefixer',
